0de8f91fe7a45d3317aa45644a08ddc3fc3593a0
dheeraj2324 <63944518+dheeraj2324@users.noreply.github.com>
2022-04-14 14:13:16 +0530

    Update bot.py

--- Bot/bot.py
+++ Bot/bot.py
@@ -81,7 +81,7 @@ def bin(update, context):
 	
              bb=res["scheme"]
              dia='✅'
-             
+             wdia='❌'
              
              
              dd=res["type"]
@@ -91,6 +91,10 @@ def bin(update, context):
              dd=res["type"]
              p=("Valid Bin! {} \n ━━━━━━━━━━━━━━━  \n • Bin: {} \n • Country: {} {} \n • Bank: {} \n • Scheme: {} \n • Type: {} \n • Currency: {} \n━━━━━━━━━━━━━━━ \n 👤 Checked By: @ASURCCWORLDBOT\n Used By @{}")
              text=p.format(dia ,ab ,res["country"]["name"],res["country"]["emoji"],res["bank"]["name"],bb,dd,res["country"]["currency"],userid)
+             Sendmessage(chat_id, text)
+        else:
+             p = "Not Valid Bin!{} \n ━━━━━━━━━━━━━━━  \n • Bin: {} \n 👤 Checked By: @ASURCCWORLDBOT\n Used By @{}"
+             text = p.format(wdia,ab,userid)
              Sendmessage(chat_id, text)
              
     else:
